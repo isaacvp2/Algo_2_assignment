6bb6e7a165215440c4767b615d9350c64aa6fcd1
isaacphilipose <isaacvp2@gmail.com>
2026-03-04 16:32:16 -0500

    added lru

--- src/main.py
+++ src/main.py
@@ -14,3 +14,20 @@ def fifo(k, m):
             cache.add(l)
             queue.append(l)
     return misses
+
+def lru(k, m):
+    cache = {}
+    misses = 0
+
+    for l in m:
+        if l in cache:
+            del cache[l]
+            cache[l] = True
+        else: 
+            misses += 1
+            if len(cache) == k:
+                deleted = next(iter(cache))
+                del cache[deleted]
+            cache[l] = True
+    return misses
+
